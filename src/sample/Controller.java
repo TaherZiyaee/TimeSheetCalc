7ef2ff85586b35efdb76a73255ed775a95201a4f
Taher Ziyaei <ziaee.taher@gmail.com>
2021-06-10 19:42:09 +0330

    added values to choiceboxes by setter methods.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,28 +1,55 @@
 package sample;
 
-import javafx.collections.FXCollections;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Controller implements Initializable {
 
-
+    @FXML
     public ChoiceBox<Integer> chbDaysOfMonth;
-//    public ChoiceBox<Integer> chbHolidays;
+    public ChoiceBox<Integer> chbHolidays;
+    public ChoiceBox<Integer> chbFridays;
+    public ChoiceBox<Integer> chbFirstDay;
 
 
     public void setChbDaysOfMonth(ChoiceBox<Integer> chbDaysOfMonth) {
-
+        chbDaysOfMonth.getItems().addAll(29,30,31);
+        chbDaysOfMonth.setValue(30);
         this.chbDaysOfMonth = chbDaysOfMonth;
+    }
+
+    public void setChbHolidays(ChoiceBox<Integer> chbHolidays) {
+        List<Integer> holidaysList = IntStream.rangeClosed(0,10).boxed().collect(Collectors.toList());
+        chbHolidays.getItems().addAll(holidaysList);
+        chbHolidays.setValue(0);
+        this.chbHolidays = chbHolidays;
+    }
+
+    public void setChbFridays(ChoiceBox<Integer> chbFridays) {
+        chbFridays.getItems().addAll(4,5);
+        chbFridays.setValue(4);
+        this.chbFridays = chbFridays;
+    }
+
+    public void setChbFirstDay(ChoiceBox<Integer> chbFirstDay) {
+        chbFirstDay.getItems().addAll(21,22);
+        chbFirstDay.setValue(21);
+        this.chbFirstDay = chbFirstDay;
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        setChbDaysOfMonth(chbDaysOfMonth);
+        setChbFirstDay(chbFirstDay);
+        setChbFridays(chbFridays);
+        setChbHolidays(chbHolidays);
     }
 }
